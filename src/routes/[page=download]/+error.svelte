96bd3e263f5ccb266ad50ca538fcbc78b7020ed9
Dan Stevens <danhstevens@gmail.com>
2023-12-12 15:18:23 -0600

    enhancing file 404 page

--- src/routes/[page=download]/+error.svelte
+++ src/routes/[page=download]/+error.svelte
@@ -2,4 +2,29 @@
   import { page } from '$app/stores';
 </script>
 
-<h1 class="text-white">{$page.status}: {$page.error?.message || ''}</h1>
+<svelte:head>
+  <title>{$page.status === 404 ? 'Upload not found' : 'Unexpected error'}</title>
+</svelte:head>
+
+<div class="my-24 text-center">
+  {#if $page.status === 404}
+    <svg class="mx-auto h-12 w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+      ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path
+        fill="#ffffff"
+        d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384v38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm59.3 107.3c6.2-6.2 6.2-16.4 0-22.6s-16.4-6.2-22.6 0L432 345.4l-36.7-36.7c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L409.4 368l-36.7 36.7c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0L432 390.6l36.7 36.7c6.2 6.2 16.4 6.2 22.6 0s6.2-16.4 0-22.6L454.6 368l36.7-36.7z" /></svg>
+    <h3 class="mt-2 text-xl font-semibold text-white">Upload not found</h3>
+    <p class="mt-1 text-sm text-zinc-500">This upload has expired, been deleted, or never existed.</p>
+  {:else}
+    <h3 class="mt-2 text-xl font-semibold text-white">An unexpected error occured</h3>
+  {/if}
+  <div class="mt-6">
+    <a
+      href="/"
+      class="inline-flex items-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+      <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+      </svg>
+      New Upload
+    </a>
+  </div>
+</div>
