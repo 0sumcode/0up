2df7f15e540e537e33fe6ea8e6888e88a35cc449
Dan Stevens <danhstevens@gmail.com>
2023-12-12 12:26:13 -0600

    adding file download ui

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
+export { getFileTypeIcon } from './getFileTypeIcon';
 export { prisma } from './Prisma';
--- src/routes/[page=download]/+page.svelte
+++ src/routes/[page=download]/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
-  import { UppyDecrypt, onUppyEncryptReady } from 'uppy-encrypt';
+  import { UppyDecrypt, onUppyEncryptReady, type DecryptedMetaData } from 'uppy-encrypt';
   import { browser } from '$app/environment';
+  import { getFileTypeIcon } from '$lib';
+  import { filesize } from 'filesize';
 
   export let data;
-  console.log(data);
+  //console.log(data);
 
+  interface FileDownload {
+    file: (typeof data.files)[number];
+    decrypt: UppyDecrypt;
+    meta: DecryptedMetaData;
+  }
+
+  const files: FileDownload[] = [];
   const decryptors: UppyDecrypt[] = [];
 
   if (browser) {
@@ -16,10 +25,11 @@
       }
 
       // Decrypt meta-data
-      for (const file of data.files) {
+      for (const i in data.files) {
+        const file = data.files[i];
         const decrypt = new UppyDecrypt(password, file.salt, file.header);
-        decryptors.push(decrypt);
-        console.log(decrypt.getDecryptedMetaData(file.meta_header, file.meta_data));
+        const meta = decrypt.getDecryptedMetaData(file.meta_header, file.meta_data);
+        files[i] = { file, decrypt, meta };
       }
     });
   }
@@ -51,3 +61,37 @@
 </script>
 
 <button class="text-white" on:click={handleDownload}>here</button>
+<span class="fiv-viv fiv-icon-pdf"></span>
+<span>{@html getFileTypeIcon('text').icon}</span>
+
+<div class="mx-auto max-w-2xl rounded-md bg-zinc-800 px-6 lg:px-8">
+  <ul role="list" class="divide-y divide-zinc-700">
+    {#if files.length}
+      {#each files as file}
+        <li class="flex items-center justify-between gap-x-6 py-5">
+          <div class="flex min-w-0 gap-x-4">
+            <div class="h-12 w-12 flex-none">
+              {@html file.meta.type ? getFileTypeIcon(file.meta.type).icon : ''}
+            </div>
+            <div class="min-w-0 flex-auto">
+              <p class="text-sm font-semibold leading-6 text-white">{file.meta.name}</p>
+              <p class="mt-1 truncate text-xs leading-5 text-zinc-500">{filesize(Number(file.file.size))}</p>
+            </div>
+          </div>
+          <a href="#" class="rounded-full bg-white/10 px-2.5 py-1 text-xs font-semibold text-white shadow-sm hover:bg-white/20">Download</a>
+        </li>
+      {/each}
+    {:else}
+      <li class="flex animate-pulse items-center justify-between gap-x-6 py-5">
+        <div class="flex min-w-0 gap-x-4">
+          <div class="flex-none">
+            {@html getFileTypeIcon('application/octet-stream').icon}
+          </div>
+          <div class="min-w-0 flex-auto">
+            <p class="text-sm font-semibold leading-6 text-white">Loading files&hellip;</p>
+          </div>
+        </div>
+      </li>
+    {/if}
+  </ul>
+</div>
