8ec1fcf34490dcdbfe4b8478bef9a1bdc7996505
Dan Stevens <danhstevens@gmail.com>
2023-12-30 13:30:32 -0600

    added copy link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,8 @@
 
   let expires: number,
     downloads: number,
-    url: string | null = null;
+    url: string | null = null,
+    copied = false;
 
   // Create/sign an upload request
   const createUpload = async (isMultipart = false) => {
@@ -34,6 +35,19 @@
 
     const data = await response.json();
     return data;
+  };
+
+  const copyLink = () => {
+    let copyText = document.getElementById('share-input');
+    if (copyText instanceof HTMLInputElement) {
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); // For mobile devices
+      navigator.clipboard.writeText(copyText.value);
+      copied = true;
+      setTimeout(() => {
+        copied = false;
+      }, 1500);
+    }
   };
 
   let uppy: Uppy;
@@ -158,7 +172,7 @@
         // Construct URL
         const data = await response.json();
         url = `${window.location.protocol}//${window.location.host}/${data.upload.replace(/-/g, '')}#${result.successful[0].meta.password}`;
-        console.log(url);
+        window.scrollTo(0, 0);
 
         // Generate new password for future upload
         uppy.getPlugin('UppyEncryptPlugin')?.setOptions({ password: UppyEncrypt.generatePassword() });
@@ -236,12 +250,12 @@
         readonly
         class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6" />
       <button
-        type="submit"
+        on:click|preventDefault={copyLink}
         class="inline-flex items-center gap-x-1.5 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-zinc-900 shadow-sm hover:bg-zinc-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
         <svg class="-ml-0.5 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
           ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path
             d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z" /></svg>
-        Copy link
+        {copied ? 'Copied!' : 'Copy link'}
       </button>
     </form>
   </div>
