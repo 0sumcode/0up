e90c6b143fd6f79a9bbc4b9ff98e8f2371e12e57
Dan Stevens <danhstevens@gmail.com>
2023-12-30 13:12:11 -0600

    adding share form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,9 @@
   const expireOptions = JSON.parse(PUBLIC_UPLOAD_EXPIRE_OPTIONS);
   const maxDownloadOptions = JSON.parse(PUBLIC_UPLOAD_MAX_DOWNLOAD_OPTIONS);
 
-  let expires: number, downloads: number;
+  let expires: number,
+    downloads: number,
+    url: string | null = null;
 
   // Create/sign an upload request
   const createUpload = async (isMultipart = false) => {
@@ -155,7 +157,7 @@
 
         // Construct URL
         const data = await response.json();
-        const url = `${window.location.protocol}//${window.location.host}/${data.upload.replace(/-/g, '')}#${result.successful[0].meta.password}`;
+        url = `${window.location.protocol}//${window.location.host}/${data.upload.replace(/-/g, '')}#${result.successful[0].meta.password}`;
         console.log(url);
 
         // Generate new password for future upload
@@ -164,61 +166,86 @@
   });
 </script>
 
-<div class="py-12">
-  <div class="mx-auto max-w-7xl px-6 lg:px-8">
-    <div class="mx-auto sm:text-center">
-      <h2 class="text-base font-semibold leading-7 text-blue-400">Free, ephemeral, <a href="#" target="_blank" class="underline">open-source</a></h2>
-      <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl"><span class="font-audiowide">0</span>-knowledge encrypted file uploads</p>
-      <!-- <p class="mt-6 text-lg leading-8 text-zinc-300">
+{#if !url}
+  <div class="py-12">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto sm:text-center">
+        <h2 class="text-base font-semibold leading-7 text-blue-400">Free, ephemeral, <a href="#" target="_blank" class="underline">open-source</a></h2>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl"><span class="font-audiowide">0</span>-knowledge encrypted file uploads</p>
+        <!-- <p class="mt-6 text-lg leading-8 text-zinc-300">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis.
       </p> -->
+      </div>
     </div>
-  </div>
-  <div class="relative overflow-hidden pt-12">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div id="uppy-dashboard"></div>
-      <div class="flex flex-col lg:flex-row">
-        <!-- First Column -->
-        <div class="lg:w-1/2">
-          <div class="p-2 text-zinc-300">
-            Expire after:
-            <select
-              bind:value={expires}
-              id="expires"
-              name="expires"
-              class="mx-2 rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 [&_*]:text-black">
-              {#each expireOptions as value}
-                <option {value}>{value} Hour{value > 1 ? 's' : ''}</option>
-              {/each}
-            </select>
-            or
-            <select
-              bind:value={downloads}
-              id="downloads"
-              name="downloads"
-              class="mx-2 rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 [&_*]:text-black">
-              {#each maxDownloadOptions as value}
-                <option {value}>{value} Download{value > 1 ? 's' : ''}</option>
-              {/each}
-            </select>
+    <div class="relative overflow-hidden pt-12">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div id="uppy-dashboard"></div>
+        <div class="flex flex-col lg:flex-row">
+          <!-- First Column -->
+          <div class="lg:w-1/2">
+            <div class="p-2 text-zinc-300">
+              Expire after:
+              <select
+                bind:value={expires}
+                id="expires"
+                name="expires"
+                class="mx-2 rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 [&_*]:text-black">
+                {#each expireOptions as value}
+                  <option {value}>{value} Hour{value > 1 ? 's' : ''}</option>
+                {/each}
+              </select>
+              or
+              <select
+                bind:value={downloads}
+                id="downloads"
+                name="downloads"
+                class="mx-2 rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 [&_*]:text-black">
+                {#each maxDownloadOptions as value}
+                  <option {value}>{value} Download{value > 1 ? 's' : ''}</option>
+                {/each}
+              </select>
+            </div>
           </div>
-        </div>
-        <!-- Second Column -->
-        <div class="lg:w-1/2">
-          <div class="p-2">
-            <!-- Content for the second column -->
-            <p class="text-right text-xs leading-6 text-zinc-300">
-              Use of this service indicates acceptance of the <a href="#" class="font-semibold text-white">terms of service</a>.
-            </p>
-            <p class="text-right text-xs leading-6 text-zinc-300">
-              We care about your data. Read our <a href="#" class="font-semibold text-white">privacy policy</a>.
-            </p>
+          <!-- Second Column -->
+          <div class="lg:w-1/2">
+            <div class="p-2">
+              <!-- Content for the second column -->
+              <p class="text-right text-xs leading-6 text-zinc-300">
+                Use of this service indicates acceptance of the <a href="#" class="font-semibold text-white">terms of service</a>.
+              </p>
+              <p class="text-right text-xs leading-6 text-zinc-300">
+                We care about your data. Read our <a href="#" class="font-semibold text-white">privacy policy</a>.
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+{:else}
+  <div class="mx-auto max-w-7xl px-6 py-12 lg:px-8">
+    <h1 class="mt-2 text-center text-3xl font-bold tracking-tight text-white">Your file(s) have been encrypted & uploaded!</h1>
+    <p class="mx-auto mt-10 max-w-xl text-center text-lg leading-8 text-zinc-300">Copy &amp; share the link below:</p>
+    <form class="mx-auto mt-2 flex max-w-xl gap-x-4">
+      <label for="share-input" class="sr-only">Share</label>
+      <input
+        id="share-input"
+        name="share"
+        type="text"
+        value={url}
+        readonly
+        class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6" />
+      <button
+        type="submit"
+        class="inline-flex items-center gap-x-1.5 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-zinc-900 shadow-sm hover:bg-zinc-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+        <svg class="-ml-0.5 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+          ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path
+            d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z" /></svg>
+        Copy link
+      </button>
+    </form>
+  </div>
+{/if}
 
 <div>
   <div class="mx-auto max-w-7xl divide-y divide-zinc-400/10 px-6 py-12 lg:px-8">
