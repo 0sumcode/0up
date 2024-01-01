956fb7164d3d35b3faf5285dfb96353cb0c48ef6
Dan Stevens <danhstevens@gmail.com>
2024-01-01 17:36:47 -0600

    nav menu cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,40 +38,8 @@
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <div class="flex h-16 justify-between">
       <div class="flex">
-        <div class="-ml-2 mr-2 flex items-center md:hidden">
-          <!-- Mobile menu button -->
-          <button
-            type="button"
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-zinc-400 hover:bg-zinc-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            aria-controls="mobile-menu"
-            aria-expanded="false">
-            <span class="absolute -inset-0.5"></span>
-            <span class="sr-only">Open main menu</span>
-            <!--
-              Icon when menu is closed.
-
-              Menu open: "hidden", Menu closed: "block"
-            -->
-            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-            <!--
-              Icon when menu is open.
-
-              Menu open: "block", Menu closed: "hidden"
-            -->
-            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
         <div class="flex flex-shrink-0 items-center">
           <div class="font-audiowide text-3xl text-white"><a href="/">0up</a></div>
-        </div>
-        <div class="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-          <!-- Current: "bg-zinc-900 text-white", Default: "text-zinc-300 hover:bg-zinc-700 hover:text-white" -->
-          <a href="#" class="rounded-md bg-zinc-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">1</a>
-          <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-zinc-300 hover:bg-zinc-700 hover:text-white">2</a>
         </div>
       </div>
       <div class="flex items-center">
@@ -92,34 +60,11 @@
       </div>
     </div>
   </div>
-
-  <!-- Mobile menu, show/hide based on menu state. -->
-  <div class="md:hidden" id="mobile-menu">
-    <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-      <!-- Current: "bg-zinc-900 text-white", Default: "text-zinc-300 hover:bg-zinc-700 hover:text-white" -->
-      <a href="#" class="block rounded-md bg-zinc-900 px-3 py-2 text-base font-medium text-white" aria-current="page">1</a>
-      <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-zinc-300 hover:bg-zinc-700 hover:text-white">2</a>
-    </div>
-  </div>
 </nav>
 <main>
   <slot />
 </main>
 
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <footer aria-labelledby="footer-heading">
   <h2 id="footer-heading" class="sr-only">Footer</h2>
   <div class="mx-auto max-w-7xl px-6 pb-8 pt-20 lg:px-8">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -268,6 +268,17 @@
         {copied ? 'Copied!' : 'Copy link'}
       </button>
     </form>
+    <div class="mt-10 flex items-center justify-center gap-x-6">
+      <a
+        href="/"
+        on:click|preventDefault={$NewUploadStore.handler}
+        class="inline-flex items-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+        <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+        </svg>
+        New Upload
+      </a>
+    </div>
   </div>
 {/if}
 
