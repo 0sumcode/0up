639be712422cfd47cc71278d43d9cf7864dbcbe0
Dan Stevens <danhstevens@gmail.com>
2024-01-05 10:52:07 -0600

    more fun with https://github.com/prisma/prisma/issues/12504

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
+    },
+  },
 });
